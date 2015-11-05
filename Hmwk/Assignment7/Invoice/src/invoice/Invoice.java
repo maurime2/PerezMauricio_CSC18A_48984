4c625b28bce37e75df23ce6b4cc79cce554258e6
maurime2 <maurime2@gmail.com>
2015-11-05 11:46:21 -0800

    Signed-off-by: maurime2 <maurime2@gmail.com>

---
+++ Hmwk/Assignment7/Invoice/src/invoice/Invoice.java
@@ -0,0 +1,42 @@
+/*
+   Mauricio S. Perez
+   CSC18a - 48984
+   Proffesor Paul J. Conrad
+   Assignment 7: Problem 7.11 (Invoice Class)
+ 
+   Problem: Chapter 7 Exercise #11 (Invoice Class), due 11/5/15 by the beginning
+            of class, working source code and 3-4 sentence write up must be 
+            emailed for up to 40 points.
+
+            7.11 (Invoice Class)
+
+            Create a class called Invoice that a hardware store might use to 
+            represent an invoice for an item sold at the store. An Invoice
+            should include four pieces of information as instance variables—a
+            part number (type String), a part description (type String), a
+            quantity of the item being purchased (type int) and a price per item
+            (double). 
+
+            Your class should have a constructor that initializes the four 
+            instance variables. Provide a set and a get method for each instance
+            variable.
+            
+            In addition, provide a method named getInvoiceAmount that calculates
+            the invoice amount (i.e., multiplies the quantity by the price per
+            item), then returns the amount as a double value. If the quantity is
+            not positive, it should be set to 0. If the price per item is not
+            positive, it should be set to 0.0. Write a test app named
+            InvoiceTest that demonstrates class Invoice’s capabilities.
+ */
+//Libraries
+package invoice;
+
+//Invoice Class
+public class Invoice {
+
+    //Main Start
+    public static void main(String[] args) {
+    
+    }//Main End
+    
+}//Invoice Class End
